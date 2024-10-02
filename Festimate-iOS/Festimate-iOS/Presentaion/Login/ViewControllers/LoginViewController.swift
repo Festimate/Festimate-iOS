00e4308f69cb027d0e69a2ccc160c711f57cab91
kms0233 <cnblue789@naver.com>
2024-10-02 15:17:05 +0900

    [Setting] 폴더링 및 컨벤션 예시 (#1)

---
+++ Festimate-iOS/Festimate-iOS/Presentaion/Login/ViewControllers/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Festimate-iOS
+//
+//  Created by 김민서 on 10/2/24.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+final class LoginViewController: UIViewController {
+    
+    // MARK: - UI Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setStyle() {
+        
+    }
+}
+
+
