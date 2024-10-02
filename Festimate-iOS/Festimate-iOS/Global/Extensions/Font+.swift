e1ebe03c9793521296d576efe4edd997832e35b3
kms0233 <cnblue789@naver.com>
2024-10-02 14:17:04 +0900

    [Setting] 폰트 extension 세팅 (#1)

---
+++ Festimate-iOS/Festimate-iOS/Global/Extensions/Font+.swift
@@ -0,0 +1,61 @@
+//
+//  Font+.swift
+//  Festimate-iOS
+//
+//  Created by 김민서 on 10/2/24.
+//
+
+import UIKit
+
+enum FontName {
+    
+    case title_extra_24, title_extra_20
+    case title_bold_20, title_bold_18, body_bold_17, body_bold_15
+    case body_semi_17, body_semi_15, body_semi_13
+    case body_med_17, body_med_15, body_med_13
+    case cap_reg_11
+    
+    
+    var rawValue: String {
+        switch self {
+        case .title_extra_24, .title_extra_20:
+            return "Pretendard-ExtraBold"
+        case .title_bold_20, .title_bold_18, .body_bold_17, .body_bold_15:
+            return "Pretendard-Bold"
+        case .body_semi_17, .body_semi_15, .body_semi_13:
+            return "Pretendard-SemiBold"
+        case .cap_reg_11:
+            return "Pretendard-Regular"
+        case .body_med_17, .body_med_15, .body_med_13:
+            return "Pretendard-Medium"
+        }
+    }
+    
+    var size: CGFloat {
+        switch self {
+        case .title_extra_24:
+            return 24
+        case .title_extra_20, .title_bold_20:
+            return 20
+        case .title_bold_18:
+            return 18
+        case .body_bold_17, .body_semi_17, .body_med_17:
+            return 17
+        case .body_bold_15, .body_semi_15, .body_med_15:
+            return 15
+        case .body_semi_13, .body_med_13:
+            return 13
+        case .cap_reg_11:
+            return 11
+        }
+    }
+}
+
+extension UIFont {
+    
+    static func pretendard(_ style: FontName) -> UIFont {
+        return UIFont(name: style.rawValue, size: style.size)!
+    }
+    
+}
+
