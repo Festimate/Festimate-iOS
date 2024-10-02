8b4d8407bfba48f5f1fbab3530b739a0a0a36861
kms0233 <cnblue789@naver.com>
2024-10-02 13:01:53 +0900

    [Setting] 스토리보드 제거 (#1)

---
+++ Festimate-iOS/Festimate-iOS/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Festimate-iOS
+//
+//  Created by 김민서 on 10/2/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        // 1.
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+    // 2.
+        self.window = UIWindow(windowScene: windowScene)
+    // 3.
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        self.window?.rootViewController = navigationController
+    // 4.
+        self.window?.makeKeyAndVisible()
+    }
+}
---
+++ Festimate-iOS/Festimate-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Festimate-iOS
+//
+//  Created by 김민서 on 10/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
