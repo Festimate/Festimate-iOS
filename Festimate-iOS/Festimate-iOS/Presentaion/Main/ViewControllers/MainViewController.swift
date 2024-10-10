976d470bef2a455fca504c18bf2e36a541ffe861
kms0233 <cnblue789@naver.com>
2024-10-10 19:11:52 +0900

    [Fix] collectionView 오른쪽 inset 값 주기 (#3)

--- Festimate-iOS/Festimate-iOS/Presentaion/Main/ViewControllers/MainViewController.swift
+++ Festimate-iOS/Festimate-iOS/Presentaion/Main/ViewControllers/MainViewController.swift
@@ -60,7 +60,7 @@ extension MainViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         
-        return UIEdgeInsets(top: 0, left: 42, bottom: 0, right: 0)
+        return UIEdgeInsets(top: 0, left: 42, bottom: 0, right: 42)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
