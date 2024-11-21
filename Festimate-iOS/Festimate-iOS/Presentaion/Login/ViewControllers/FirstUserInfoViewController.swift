58db6faee4869287cf40ef0f67598917e1c07717
sangwook <operawook@gmail.com>
2024-11-21 10:20:37 +0900

    [Feat] first userinfo view (#9)

---
+++ Festimate-iOS/Festimate-iOS/Presentaion/Login/ViewControllers/FirstUserInfoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstUserInfoViewController.swift
+//  Festimate-iOS
+//
+//  Created by 우상욱 on 11/20/24.
+//
+
+import UIKit
+
+class FirstUserInfoViewController: UIViewController {
+
+    // MARK: - UI Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+
+    func setHierarchy() {
+    }
+    
+    func setLayout() {
+    }
+    
+    func setStyle() {
+        self.view.backgroundColor = .mainCoral
+    }
+}
