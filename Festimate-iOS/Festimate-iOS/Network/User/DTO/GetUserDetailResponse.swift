516de286db5206c63588a59e0cd7ab5dc9b71d0d
kms0233 <cnblue789@naver.com>
2024-11-28 03:00:43 +0900

    [Feat] GetUserDetailResponse 구현 (#16)

---
+++ Festimate-iOS/Festimate-iOS/Network/User/DTO/GetUserDetailResponse.swift
@@ -0,0 +1,14 @@
+//
+//  GetUserDetailResponse.swift
+//  Festimate-iOS
+//
+//  Created by 김민서 on 11/28/24.
+//
+
+import Foundation
+
+struct GetUserDetailResponse: Codable {
+    let nickname: String
+    let school: String
+}
+
