48668e2e783f12b680196be1386c8d41a78d8ccb
kms0233 <cnblue789@naver.com>
2024-11-28 03:45:24 +0900

    [Feat] MatchingTargetType 구현 (#18)

---
+++ Festimate-iOS/Festimate-iOS/Network/Matching/MatchingTargetType.swift
@@ -0,0 +1,47 @@
+//
+//  MatchingTargetType.swift
+//  Festimate-iOS
+//
+//  Created by 김민서 on 11/28/24.
+//
+
+import Foundation
+
+import Moya
+
+enum MatchingTargetType {
+    case postMatching(matching: PostMatchingRequest)
+}
+
+extension MatchingTargetType: BaseTargetType {
+    var utilPath: String {
+        return "festimate/v1/matching"
+    }
+    
+    var path: String {
+        switch self {
+        case .postMatching:
+            return utilPath
+        }
+    }
+    
+    var method: Moya.Method {
+        return .post
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .postMatching(let matching):
+            return .requestJSONEncodable(matching)
+        }
+    }
+    
+    var headers: [String: String]? {
+        return [
+            "Content-Type": "application/json",
+            "userId": "1"
+        ]
+    }
+}
+
+
