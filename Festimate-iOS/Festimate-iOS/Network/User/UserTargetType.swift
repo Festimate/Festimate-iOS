aa5bb5239973eaafd945400d050f3c4f8fb0c068
kms0233 <cnblue789@naver.com>
2024-11-28 03:01:12 +0900

    [Feat] UserTargetType 구현 (#16)

---
+++ Festimate-iOS/Festimate-iOS/Network/User/UserTargetType.swift
@@ -0,0 +1,45 @@
+//
+//  UserTargetType.swift
+//  Festimate-iOS
+//
+//  Created by 김민서 on 11/28/24.
+//
+
+import Foundation
+import Moya
+
+enum UserTargetType {
+    case getUserDetail
+}
+
+extension UserTargetType: BaseTargetType {
+    var utilPath: String {
+        return "festimate/v1/user-detail"
+    }
+    
+    var path: String {
+        switch self {
+        case .getUserDetail:
+            return utilPath
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getUserDetail:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        return [
+            "Content-Type": "application/json",
+            "userId": "1"
+        ]
+    }
+}
+
